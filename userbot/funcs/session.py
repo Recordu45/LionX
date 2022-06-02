58e0043954094be3894f3f6f90dfacc1a267d707
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-02 10:01:40 +0530

    Update session.py

--- userbot/funcs/session.py
+++ userbot/funcs/session.py
@@ -10,13 +10,18 @@ __version__ = "2.10.6"
 
 loop = None
 
-if Config.STRING_SESSION:
-    session = StringSession(str(Config.STRING_SESSION))
-else:
-    session = "LionX"
 
-try:
-    lionub = LionXClient(
+session = StringSession(str(Config.STRING_SESSION))
+session2 = StringSession(str(Config.STRING_SESSION2))
+session3 = StringSession(str(Config.STRING_SESSION3))
+session4 = StringSession(str(Config.STRING_SESSION4))
+session5 = StringSession(str(Config.STRING_SESSION5))
+
+
+
+
+if Config.STRING_SESSION:
+    bot1 = LionXClient(
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
@@ -26,9 +31,64 @@ try:
         auto_reconnect=True,
         connection_retries=None,
     )
-except Exception as e:
-    print(f"STRING_SESSION - {e}")
-    sys.exit()
+else:
+    bot1 = None
+
+if Config.STRING_SESSION2:
+    bot2 = LionXClient(
+        session=session2,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    bot2 = None
+
+if Config.STRING_SESSION3:
+    bot3 = LionXClient(
+        session=session3,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    bot3 = None
+
+if Config.STRING_SESSION4:
+    bot4 = LionXClient(
+        session=session4,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    bot4 = None
+
+if Config.STRING_SESSION5:
+    bot5 = LionXClient(
+        session=session5,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    bot5 = None
 
 
 lionub.tgbot = tgbot = LionXClient(
